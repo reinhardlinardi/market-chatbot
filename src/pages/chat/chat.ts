e4bc2dcd79ebff6dfc850a1882a73d419e3b6769
Reinhard Linardi <reinhardlinardi@gmail.com>
2017-11-18 18:37:56 +0700

    Add chatbox to chatroom + fix chatbox css

--- src/pages/chat/chat.ts
+++ src/pages/chat/chat.ts
@@ -9,6 +9,7 @@ import { NavController, NavParams } from 'ionic-angular';
  * Ionic pages and navigation.
  */
 
+
 /* Create model to be passed to view */
 class Command {
   val: string;
@@ -24,17 +25,27 @@ export class ChatPage {
 
   /* Properties */
   command: Command; // Add model as property of class
+  public items: any[] = []; // Chatboxes
+  
+  /* Constants */
+  greeting: string = "Hai, selamat datang! Terima kasih telah menambahkan saya sebagai teman. Ketik \"bantuan\" untuk melihat perintah apa saja yang tersedia.";
 
   /* Methods */
   constructor(public navCtrl: NavController, public navParams: NavParams) {
     this.command = new Command(); // Create new instance of model in Constructor
+
+    // Send greeting message
+    this.items.push({ container: "chatbox-container-bot", type: "chatbox-bot", chat: this.greeting });
   }
 
+  // Evaluate command given by user
   runCommand(command : Command) {
     
     // If command.val is not falsy
     if(command.val) {
-      alert('Command : ' + command.val);
+      // do something
+      this.items.push({ container: "chatbox-container-user", type: "chatbox-user", chat: command.val});
+      command.val = ""; // reset text
     }
   }
 }
