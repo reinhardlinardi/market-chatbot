cfad5c8a04169a439dff06304e90bd7b69b74d7a
Reinhard Linardi <reinhardlinardi@gmail.com>
2017-11-19 00:15:23 +0700

    Add initial help and error message

--- src/pages/chat/chat.ts
+++ src/pages/chat/chat.ts
@@ -29,7 +29,9 @@ export class ChatPage {
   public items: any[] = []; // Chatboxes
   
   /* Constants */
-  greeting: string = "Hai, selamat datang! Terima kasih telah menambahkan saya sebagai teman. Ketik \"bantuan\" untuk melihat perintah apa saja yang tersedia.";
+  greeting: string = "Halo! Selamat datang! Terima kasih telah menambahkan saya sebagai teman. Ketik \"Bantuan\" untuk melihat perintah apa saja yang tersedia.";
+  help: string = "-- Bantuan -- \n\nGunakan keyword yang tersedia di bawah ini. \n\nCari : Mencari barang \n\nContoh : Cari kamera";
+  error: string = "Keyword yang anda masukkan salah. Silahkan coba lagi.";
 
   /* Methods */
   constructor(public navCtrl: NavController, public navParams: NavParams) {
@@ -45,12 +47,27 @@ export class ChatPage {
     // If command.val is not falsy
     if(command.val) {
       this.items.push({ container: "chatbox-container-user", type: "chatbox-user", chat: command.val});
-      command.val = ""; // reset text
 
       // Scroll to latest chat
       this.content.scrollTo(0, this.content.scrollHeight); // scroll to bottom of content
 
       // Bot action
+      this.botAction(command.val);
+
+      // Reset model (and view, via data binding)
+      command.val = "";
     }
+  }
+
+  botAction(input: string) {
+    let lower_input: string = input.toLowerCase(); // convert to lowercase first
+    
+    // Regex for string matching
+    let re_bantuan = /bantuan/;
+
+    if(lower_input.search(re_bantuan) == -1) {
+      this.items.push({ container: "chatbox-container-bot", type: "chatbox-bot", chat: this.error });
+    }
+    else this.items.push({ container: "chatbox-container-bot", type: "chatbox-bot", chat: this.help });
   }
 }
