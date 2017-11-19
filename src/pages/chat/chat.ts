0484b9ac675eeaab4b9e7bdc7e22fd009c14f087
Reinhard Linardi <reinhardlinardi@gmail.com>
2017-11-19 19:12:11 +0700

    Display list in chatbox for address

--- src/pages/chat/chat.ts
+++ src/pages/chat/chat.ts
@@ -1,6 +1,7 @@
 import { Component, ViewChild } from '@angular/core';
 /* import { IonicPage, NavController, NavParams } from 'ionic-angular'; */
-import { NavController, NavParams, Content } from 'ionic-angular';
+import { NavController, NavParams } from 'ionic-angular';
+import { Content } from 'ionic-angular';
 
 /**
  * Generated class for the ChatPage page.
@@ -26,24 +27,31 @@ export class ChatPage {
 
   /* Properties */
   command: Command; // Add model as property of class
-  public chatboxes: any[] = []; // Chatboxes
+  public chatboxes: any[] = []; // Array of chatboxes
+  public address: string[] = ["jkt","bdg"]; // Array of address
   
   /* Constants */
   greeting: string = "Halo! Selamat datang! Terima kasih telah menambahkan saya sebagai teman. Ketik \"Bantuan\" untuk melihat perintah apa saja yang tersedia.";
-  help: string = "-- Bantuan --\n\nGunakan perintah yang tersedia di bawah ini.\n\n"
-                  + "<b>Cari :</b>\nMencari barang\n"
-                  + "<b>Keranjang :</b>\nMenampilkan semua barang di keranjang\n"
-                  + "<b>Pembayaran :</b>\nMelanjutkan ke pembayaran\n"
-                  + "<b>Metode Pembayaran :</b>\nMenampilkan atau mengubah metode pembayaran\n"
-                  + "<b>Alamat Tujuan :</b>\nMenampilkan atau mengubah alamat tujuan\n\n"
-                  + "Contoh penggunaan perintah :\n\n"
+  help: string = "-- Bantuan --\n\nAnda dapat menggunakan perintah yang tersedia di bawah ini.\n\n"
+                  + "<b>Cari :</b>\nMencari barang berdasarkan nama barang\n"
+                  + "<b>Keranjang :</b>\nMenampilkan semua barang yang ada di keranjang\n"
+                  + "<b>Bayar :</b>\nMelanjutkan ke tahap pembayaran\n"
+                  + "<b>Metode :</b>\nMenampilkan atau mengubah metode pembayaran\n"
+                  + "<b>Alamat :</b>\nMenampilkan atau mengubah alamat kirim\n\n"
+                  + "Contoh penggunaan perintah sebagai berikut :\n\n"
                   + "<b>Cari :</b> <u>Cari</u> kamera\n"
                   + "<b>Keranjang :</b> <u>Keranjang</u>\n"
-                  + "<b>Pembayaran :</b> <u>Pembayaran</u>\n"
-                  + "<b>Metode Pembayaran :</b> <u>Metode Pembayaran</u>\n"
-                  + "<b>Alamat Tujuan :</b> <u>Alamat Tujuan</u>\n";
+                  + "<b>Bayar :</b> <u>Bayar</u>\n"
+                  + "<b>Metode :</b> <u>Metode</u>\n"
+                  + "<b>Alamat :</b> <u>Alamat</u>\n";
 
-
+  address_header: string = "-- Alamat Kirim --\n\nDaftar alamat kirim Anda yang tersimpan saat ini :\n\n";
+  address_footer: string = "\nAnda dapat menggunakan perintah di bawah ini untuk menambah atau menghapus alamat kirim.\n\n"
+                            + "<b>Tambah :</b>\nMenambahkan alamat kirim baru\n"
+                            + "<b>Hapus :</b>\nMenghapus alamat kirim\n\n"
+                            + "Contoh penggunaan perintah sebagai berikut :\n\n"
+                            + "<b>Tambah :</b> <u>Tambah</u>\n"
+                            + "<b>Hapus :</b> <u>Hapus</u>\n";
   error: string = "Perintah yang anda masukkan salah. Silahkan coba lagi atau ketik \"Bantuan\" untuk melihat perintah apa saja yang tersedia.";
 
   /* Methods */
@@ -51,7 +59,7 @@ export class ChatPage {
     this.command = new Command(); // Create new instance of model in Constructor
 
     // Send greeting message
-    this.chatboxes.push({ container: "chatbox-container-bot", type: "chatbox-bot", chat: this.greeting });
+    this.chatboxes.push({ container: "chatbox-container-bot", type: "chatbox-bot", content: "text", data: this.greeting });
   }
 
   // Evaluate command given by user
@@ -60,9 +68,9 @@ export class ChatPage {
     // If command.val is not falsy
     if(command.val) {
       this.content.resize(); // Recalculate content dimensions
-      if(this.content.scrollHeight > this.content.contentHeight ) this.content.scrollTo(0, this.content.scrollHeight); // Scroll to user's latest chat if content has a scroll
+      if(this.content.scrollHeight > this.content.contentHeight) this.content.scrollTo(0, this.content.scrollHeight); // Scroll to user's latest chat if content has a scroll
 
-      this.chatboxes.push({ container: "chatbox-container-user", type: "chatbox-user", chat: command.val}); // Show user's chatbox
+      this.chatboxes.push({ container: "chatbox-container-user", type: "chatbox-user", content: "text", data: command.val}); // Show user's chatbox
       this.botAction(command.val); // Bot action
       command.val = ""; // Reset model (and view, via data binding)
     }
@@ -74,12 +82,25 @@ export class ChatPage {
     
     // Regex for string matching
     let re_bantuan = /^\s*bantuan\s*$/;
+    let re_cari = /^\s*cari\s*$/;
+    let re_keranjang = /^\s*keranjang\s*$/;
+    let re_bayar = /^\s*bayar\s*$/;
+    let re_metode = /^\s*metode\s*$/;
+    let re_alamat = /^\s*alamat\s*$/;
     
     // Match each keyword
     let match_bantuan = re_bantuan.exec(lower_input);
+    let match_cari = re_cari.exec(lower_input);
+    let match_keranjang = re_keranjang.exec(lower_input);
+    let match_bayar = re_bayar.exec(lower_input);
+    let match_metode = re_metode.exec(lower_input);
+    let match_alamat = re_alamat.exec(lower_input);
 
     // Decide response
-    if(match_bantuan != null) this.chatboxes.push({ container: "chatbox-container-bot", type: "chatbox-bot", chat: this.help }); // Menu bantuan
-    else this.chatboxes.push({ container: "chatbox-container-bot", type: "chatbox-bot", chat: this.error });
+    if(match_bantuan != null) this.chatboxes.push({ container: "chatbox-container-bot", type: "chatbox-bot", content: "text", data: this.help }); // Menu bantuan
+    else if(match_alamat != null){
+      this.chatboxes.push({ container: "chatbox-container-bot", type: "chatbox-bot", content: "list", header: this.address_header, footer: this.address_footer, data: "address" });
+    }
+    else this.chatboxes.push({ container: "chatbox-container-bot", type: "chatbox-bot", content: "text", data: this.error });
   }
 }
