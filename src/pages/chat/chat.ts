398c085e9aba7e0f4c36678320041af93e6c5cce
Reinhard Linardi <reinhardlinardi@gmail.com>
2017-11-19 21:25:23 +0700

    Add input new address

--- src/pages/chat/chat.ts
+++ src/pages/chat/chat.ts
@@ -1,7 +1,7 @@
 import { Component, ViewChild } from '@angular/core';
 /* import { IonicPage, NavController, NavParams } from 'ionic-angular'; */
 import { NavController, NavParams } from 'ionic-angular';
-import { Content } from 'ionic-angular';
+import { Content, AlertController } from 'ionic-angular';
 
 /**
  * Generated class for the ChatPage page.
@@ -28,34 +28,38 @@ export class ChatPage {
   /* Properties */
   command: Command; // Add model as property of class
   public chatboxes: any[] = []; // Array of chatboxes
-  public address: string[] = ["jkt","bdg"]; // Array of address
+  stack: string[] = []; // Command stack
+
+  public address: string[] = []; // Array of address
   
   /* Constants */
-  greeting: string = "Halo! Selamat datang! Terima kasih telah menambahkan saya sebagai teman. Ketik \"Bantuan\" untuk melihat perintah apa saja yang tersedia.";
+  greeting: string = "Halo! Selamat datang! Saya adalah chatbot yang dapat membantu kamu berbelanja. Ketik \"Bantuan\" untuk melihat perintah apa saja yang tersedia.";
   help: string = "-- Bantuan --\n\nAnda dapat menggunakan perintah yang tersedia di bawah ini.\n\n"
-                  + "<b>Cari :</b>\nMencari barang berdasarkan nama barang\n"
-                  + "<b>Keranjang :</b>\nMenampilkan semua barang yang ada di keranjang\n"
-                  + "<b>Bayar :</b>\nMelanjutkan ke tahap pembayaran\n"
-                  + "<b>Metode :</b>\nMenampilkan atau mengubah metode pembayaran\n"
-                  + "<b>Alamat :</b>\nMenampilkan atau mengubah alamat kirim\n\n"
+                  + "<b>Cari :</b>\nMencari barang berdasarkan nama barang.\n"
+                  + "<b>Keranjang :</b>\nMenampilkan semua barang yang ada di keranjang.\n"
+                  + "<b>Bayar :</b>\nMelanjutkan ke tahap pembayaran.\n"
+                  + "<b>Metode :</b>\nMenampilkan atau mengubah metode pembayaran.\n"
+                  + "<b>Alamat :</b>\nMenampilkan atau mengubah alamat kirim.\n"
+                  + "<b>Batal : </b>\nMembatalkan pilihan, kembali ke pilihan sebelumnya.\n\n"
                   + "Contoh penggunaan perintah sebagai berikut :\n\n"
                   + "<b>Cari :</b> <u>Cari</u> kamera\n"
                   + "<b>Keranjang :</b> <u>Keranjang</u>\n"
                   + "<b>Bayar :</b> <u>Bayar</u>\n"
                   + "<b>Metode :</b> <u>Metode</u>\n"
-                  + "<b>Alamat :</b> <u>Alamat</u>\n";
+                  + "<b>Alamat :</b> <u>Alamat</u>\n"
+                  + "<b>Batal :</b> <u>Batal</u>\n";
 
-  address_header: string = "-- Alamat Kirim --\n\nDaftar alamat kirim Anda yang tersimpan saat ini :\n\n";
+  address_header: string = "-- Alamat Kirim --\n\nDaftar alamat kirim Anda :\n\n";
   address_footer: string = "\nAnda dapat menggunakan perintah di bawah ini untuk menambah atau menghapus alamat kirim.\n\n"
-                            + "<b>Tambah :</b>\nMenambahkan alamat kirim baru\n"
-                            + "<b>Hapus :</b>\nMenghapus alamat kirim\n\n"
+                            + "<b>Tambah :</b>\nMenambahkan alamat kirim baru. Jika alamat sudah terdaftar, alamat tidak akan ditambahkan ke daftar alamat kirim.\n"
+                            + "<b>Hapus :</b>\nMenghapus alamat kirim.\n\n"
                             + "Contoh penggunaan perintah sebagai berikut :\n\n"
                             + "<b>Tambah :</b> <u>Tambah</u>\n"
                             + "<b>Hapus :</b> <u>Hapus</u>\n";
   error: string = "Perintah yang anda masukkan salah. Silahkan coba lagi atau ketik \"Bantuan\" untuk melihat perintah apa saja yang tersedia.";
 
   /* Methods */
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, private alertCtrl: AlertController) {
     this.command = new Command(); // Create new instance of model in Constructor
 
     // Send greeting message
@@ -87,6 +91,9 @@ export class ChatPage {
     let re_bayar = /^\s*bayar\s*$/;
     let re_metode = /^\s*metode\s*$/;
     let re_alamat = /^\s*alamat\s*$/;
+    let re_batal = /^\s*batal\s*$/;
+    let re_tambah = /^\s*tambah\s*$/;
+    let re_hapus = /^\s*hapus\s*$/;
     
     // Match each keyword
     let match_bantuan = re_bantuan.exec(lower_input);
@@ -95,12 +102,61 @@ export class ChatPage {
     let match_bayar = re_bayar.exec(lower_input);
     let match_metode = re_metode.exec(lower_input);
     let match_alamat = re_alamat.exec(lower_input);
+    let match_batal = re_batal.exec(lower_input);
+    let match_tambah = re_tambah.exec(lower_input);
+    let match_hapus = re_hapus.exec(lower_input);
 
     // Decide response
+    if(match_batal != null) {
+      this.stack = [];
+      // set match_something = true
+    }
+
     if(match_bantuan != null) this.chatboxes.push({ container: "chatbox-container-bot", type: "chatbox-bot", content: "text", data: this.help }); // Menu bantuan
-    else if(match_alamat != null){
+    else if(match_alamat != null) {
+      this.stack = [];
+      this.stack.push("alamat");
       this.chatboxes.push({ container: "chatbox-container-bot", type: "chatbox-bot", content: "list", header: this.address_header, footer: this.address_footer, data: "address" });
     }
+    else if(match_tambah != null) {
+      let prev_command: string = this.stack[this.stack.length - 1];
+
+      if(prev_command == "alamat") this.addAddress();
+      else this.chatboxes.push({ container: "chatbox-container-bot", type: "chatbox-bot", content: "text", data: "Tidak ada data yang dapat ditambahkan." });
+    }
     else this.chatboxes.push({ container: "chatbox-container-bot", type: "chatbox-bot", content: "text", data: this.error });
+  }
+
+  newValidAddress(new_address: string) {
+    return !(this.address.indexOf(new_address) > -1) && (new_address != "");
+  }
+
+  addAddress() {
+    let alert = this.alertCtrl.create({
+      title: 'Tambah Alamat Kirim',
+      inputs: [{
+          name: 'address',
+          placeholder: 'Alamat kirim baru'
+        }],
+      buttons: [{
+          text: 'Batal',
+          role: 'cancel'
+        },
+        {
+          text: 'Tambah',
+          handler: data => {
+            if(this.newValidAddress(data.address)) {
+              this.address.push(data.address);
+              this.chatboxes.push({ container: "chatbox-container-bot", type: "chatbox-bot", content: "text", data: "Alamat berhasil ditambahkan." });
+            } else this.chatboxes.push({ container: "chatbox-container-bot", type: "chatbox-bot", content: "text", data: "Alamat sudah terdaftar. Tidak ada alamat baru yang ditambahkan pada daftar alamat kirim." });
+
+            this.content.resize(); // Recalculate content dimensions
+            if(this.content.scrollHeight > this.content.contentHeight) this.content.scrollTo(0, this.content.scrollHeight); // Scroll to user's latest chat if content has a scroll
+          }
+        }
+      ]
+    });
+
+    alert.present();
   }
 }
